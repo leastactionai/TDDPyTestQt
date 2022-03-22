7e4defbce3aead8d89c194cd26c1415055a84eb7
LeastAction <least.action.ai@gmail.com>
2022-03-21 22:06:12 -0700

    First, really crappy, version of refactoring tests

--- test_movie_list.py
+++ test_movie_list.py
@@ -3,23 +3,25 @@ import pytest
 from movie_list import MovieList
 from movie import Movie
 
-
-def test_empty_list_size():
-    empty_list = MovieList()
-    assert empty_list.size() == 0, "Size of empty movie list should be 0."
-
-
-def test_size_after_adding_one():
-    star_wars = Movie()
-    one_item_list = MovieList()
-    one_item_list.add(star_wars)
-    assert one_item_list.size() == 1, "Size of one item list should be 1."
-
-
-def test_size_after_adding_two():
+@pytest.fixture()
+def setup():
+    print("------------ SETUP ----------------")
+    movie_list = MovieList()
     star_wars = Movie()
     star_trek = Movie()
-    two_item_list = MovieList()
-    two_item_list.add(star_wars)
-    two_item_list.add(star_trek)
-    assert two_item_list.size() == 2, "Size of two item list should be 2."
+    yield movie_list, star_wars, star_trek
+    print("----- TEARDOWN -----")
+
+
+class TestMovieList():
+    def test_empty_list_size(self, setup):
+        assert setup[0].size() == 0, "Size of empty movie list should be 0."
+
+    def test_size_after_adding_one(self, setup):
+        setup[0].add(setup[1])
+        assert setup[0].size() == 1, "Size of one item list should be 1."
+
+    def test_size_after_adding_two(self, setup):
+        setup[0].add(setup[1])
+        setup[0].add(setup[2])
+        assert setup[0].size() == 2, "Size of two item list should be 2."
