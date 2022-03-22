8a4687f5849f7b49c6859bbd849617453188604d
LeastAction <least.action.ai@gmail.com>
2022-03-21 21:32:18 -0700

    Test 1-1-2 (Test 2) passes

--- movie_list.py
+++ movie_list.py
@@ -1,4 +1,10 @@
 class MovieList():
+    def __init__(self) -> None:
+        self.number_of_movies = 0
 
     def size(self) -> int:
-        return 0
+        return self.number_of_movies
+
+    def add(self, movie_to_add) -> None:
+        self.number_of_movies = 1
+
--- test_movie_list.py
+++ test_movie_list.py
@@ -1,7 +1,14 @@
 import pytest
 
 from movie_list import MovieList
+from movie import Movie
 
 def test_empty_list_size():
     empty_list = MovieList()
     assert empty_list.size() == 0, "Size of empty movie list should be 0."
+
+def test_size_after_adding_one():
+    star_wars = Movie()
+    one_item_list = MovieList()
+    one_item_list.add(star_wars)
+    assert one_item_list.size() == 1, "Size of one item list should be 1."
